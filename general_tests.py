a1b55726a203e039627aef866356a2e67f1dfe2d
ikram_c <68299933+Ikram-c@users.noreply.github.com>
2023-02-27 00:37:11 +0000

    tested column names which helped generate an ERD on lucid

--- general_tests.py
+++ general_tests.py
@@ -1,15 +1,22 @@
-import os
-import glob
+import pandas as pd
 
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-
-
-
-
-path = 'C:\Users\ikram\Documents\GitHub\Course-Recommendor-System-IBM-'
-extension = 'csv'
-os.chdir(dir_path)
-result = glob.glob('*.{}'.format(extension))
-print(result)
+#course_embeddings
+course_embeddings = pd.read_csv('course_embeddings.csv')
+#course_genre
+course_genre = pd.read_csv('course_genre.csv')
+#course_processed
+course_processed = pd.read_csv('course_processed.csv')
+#course_ratings
+course_ratings = pd.read_csv('course_ratings.csv')
+#courses_bows
+courses_bows = pd.read_csv('courses_bows.csv')
+#ratings
+ratings = pd.read_csv('ratings.csv')
+#rs_content_test
+rs_content_test = pd.read_csv('rs_content_test.csv')
+#sim
+sim = pd.read_csv('sim.csv')
+#user_embeddings
+user_embeddings = pd.read_csv('user_embeddings.csv')
+#user_profile
+user_profile = pd.read_csv('user_profile.csv')
